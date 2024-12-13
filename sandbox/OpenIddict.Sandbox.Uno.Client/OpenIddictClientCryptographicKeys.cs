1b13424c0cf1c1a1edc0eeecdf750bdffbeca63b
Andrii Chebukin <XperiAndri@Outlook.com>
2024-12-13 20:37:54 +0400

    Implemented targets filtering depending on build agent

--- sandbox/OpenIddict.Sandbox.Uno.Client/OpenIddictClientCryptographicKeys.cs
+++ sandbox/OpenIddict.Sandbox.Uno.Client/OpenIddictClientCryptographicKeys.cs
@@ -7,12 +7,13 @@ using System.Runtime.InteropServices;
 using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
-#if IOS || MACCATALYST
-    using Apple.CryptoKit;
-    using Foundation;
-#endif
 using Microsoft.IdentityModel.Tokens;
 using Uno;
+
+#if IOS || MACCATALYST
+using Apple.CryptoKit;
+using Foundation;
+#endif
 
 internal static class OpenIddictClientCryptographicKeys
 {
@@ -42,32 +43,32 @@ internal static class OpenIddictClientCryptographicKeys
         return new RsaSecurityKey(myRsa);
 #else
         System.Security.Cryptography.CngKey key;
-        #pragma warning disable CA2000 // Dispose objects before losing scope
-                if (CngKey.Exists(name, provider, CngKeyOpenOptions.UserKey))
-                {
-                    key = CngKey.Open(name, provider, CngKeyOpenOptions.UserKey);
-                }
+#pragma warning disable CA2000 // Dispose objects before losing scope
+        if (CngKey.Exists(name, provider, CngKeyOpenOptions.UserKey))
+        {
+            key = CngKey.Open(name, provider, CngKeyOpenOptions.UserKey);
+        }
 
-                else
+        else
+        {
+            try
+            {
+                key = CngKey.Create(CngAlgorithm.Rsa, name, new CngKeyCreationParameters
                 {
-                    try
-                    {
-                        key = CngKey.Create(CngAlgorithm.Rsa, name, new CngKeyCreationParameters
-                        {
-                            KeyCreationOptions = CngKeyCreationOptions.None,
-                            KeyUsage = usages,
-                            Parameters = { new CngProperty("Length", BitConverter.GetBytes(2048), CngPropertyOptions.None) },
-                            Provider = provider
-                        });
-                    }
+                    KeyCreationOptions = CngKeyCreationOptions.None,
+                    KeyUsage = usages,
+                    Parameters = { new CngProperty("Length", BitConverter.GetBytes(2048), CngPropertyOptions.None) },
+                    Provider = provider
+                });
+            }
 
-                    // If multiple instances of the application were started at the same time, a race condition
-                    // might occur here. In this case, try to open the key that was created by the other instance.
-                    catch (CryptographicException) when (CngKey.Exists(name, provider, CngKeyOpenOptions.UserKey))
-                    {
-                        key = CngKey.Open(name, provider, CngKeyOpenOptions.UserKey);
-                    }
-                }
+            // If multiple instances of the application were started at the same time, a race condition
+            // might occur here. In this case, try to open the key that was created by the other instance.
+            catch (CryptographicException) when (CngKey.Exists(name, provider, CngKeyOpenOptions.UserKey))
+            {
+                key = CngKey.Open(name, provider, CngKeyOpenOptions.UserKey);
+            }
+        }
 
         return new RsaSecurityKey(new RSACng(key));
 #pragma warning restore CA2000 // Dispose objects before losing scope
