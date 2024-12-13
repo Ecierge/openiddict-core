1a3cf8e2e38ba6da9348124138d088568474ae7c
Oleksii Bailo <oleksii.bailo@belightsoft.com>
2024-12-12 23:58:46 -0500

    Initial implementation of key management in Keychain(Apple)

--- sandbox/OpenIddict.Sandbox.Uno.Client/OpenIddictClientCryptographicKeys.cs
+++ sandbox/OpenIddict.Sandbox.Uno.Client/OpenIddictClientCryptographicKeys.cs
@@ -3,71 +3,71 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
-
+using Apple.CryptoKit;
+using Foundation;
 using Microsoft.IdentityModel.Tokens;
+using Uno;
 
 internal static class OpenIddictClientCryptographicKeys
 {
     internal static RsaSecurityKey GetRsaCngKey(string name, CngKeyUsages usages, CngProvider provider)
     {
         name = name ?? throw new ArgumentNullException(nameof(name));
+#if IOS || MACCATALYST
 
-        System.Security.Cryptography.CngKey key;
+        NSData rawKeyData;
 
-#pragma warning disable CA2000 // Dispose objects before losing scope
-        if (CngKey.Exists(name, provider, CngKeyOpenOptions.UserKey))
+        if (ACKeychainStorage.IsKeyExist(name, false))
         {
-            key = CngKey.Open(name, provider, CngKeyOpenOptions.UserKey);
+            rawKeyData = ACKeychainStorage.GetKey(name, false);
         }
-
         else
         {
-            try
-            {
-                key = CngKey.Create(CngAlgorithm.Rsa, name, new CngKeyCreationParameters
-                {
-                    KeyCreationOptions = CngKeyCreationOptions.None,
-                    KeyUsage = usages,
-                    Parameters = { new CngProperty("Length", BitConverter.GetBytes(2048), CngPropertyOptions.None) },
-                    Provider = provider
-                });
-            }
-
-            // If multiple instances of the application were started at the same time, a race condition
-            // might occur here. In this case, try to open the key that was created by the other instance.
-            catch (CryptographicException) when (CngKey.Exists(name, provider, CngKeyOpenOptions.UserKey))
-            {
-                key = CngKey.Open(name, provider, CngKeyOpenOptions.UserKey);
-            }
+            rawKeyData = ACKeychainStorage.CreateKey(name, "rsa", 2048, false, !(usages == CngKeyUsages.Signing), true);
         }
+
+        int bytesRead = 0;
+        byte[] keyData = new byte[rawKeyData.Length];
+        Marshal.Copy(rawKeyData.Bytes, keyData, 0, (int)rawKeyData.Length);
+
+        RSA myRsa = RSA.Create();
+        myRsa.ImportRSAPrivateKey(keyData, out bytesRead);
+
+        return new RsaSecurityKey(myRsa);
+#else
+        #pragma warning disable CA2000 // Dispose objects before losing scope
+                if (CngKey.Exists(name, provider, CngKeyOpenOptions.UserKey))
+                {
+                    key = CngKey.Open(name, provider, CngKeyOpenOptions.UserKey);
+                }
+
+                else
+                {
+                    try
+                    {
+                        key = CngKey.Create(CngAlgorithm.Rsa, name, new CngKeyCreationParameters
+                        {
+                            KeyCreationOptions = CngKeyCreationOptions.None,
+                            KeyUsage = usages,
+                            Parameters = { new CngProperty("Length", BitConverter.GetBytes(2048), CngPropertyOptions.None) },
+                            Provider = provider
+                        });
+                    }
+
+                    // If multiple instances of the application were started at the same time, a race condition
+                    // might occur here. In this case, try to open the key that was created by the other instance.
+                    catch (CryptographicException) when (CngKey.Exists(name, provider, CngKeyOpenOptions.UserKey))
+                    {
+                        key = CngKey.Open(name, provider, CngKeyOpenOptions.UserKey);
+                    }
+                }
 
         return new RsaSecurityKey(new RSACng(key));
 #pragma warning restore CA2000 // Dispose objects before losing scope
-    }
-}
-
-#if IOS || MACCATALYST
-
-public static class CngKey
-{
-    public static bool Exists(string name, CngProvider provider, CngKeyOpenOptions options)
-    {
-        return false;
-    }
-
-    public static System.Security.Cryptography.CngKey Open(string name, CngProvider provider, CngKeyOpenOptions options)
-    {
-        //System.Security.Cryptography.CgnKey.Import();
-        return null;
-    }
-
-    public static System.Security.Cryptography.CngKey Create(CngAlgorithm algorithm, string name, CngKeyCreationParameters parameters)
-    {
-        //System.Security.Cryptography.CgnKey.Import();
-        return null;
-    }
-}
 #endif
+    }
+}
